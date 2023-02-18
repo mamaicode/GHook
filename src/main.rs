15e9ab38ba0bd4dbbf6aa6abd2404efb2d9ee3ba
scirin <irine.kokilashvili@gmail.com>
2023-02-18 16:27:51 +0000

    test: webhook1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use ntex::web::{self, HttpResponse, HttpRequest};
+
+#[web::post("/webhook")]
+async fn webhook(
+  req: HttpRequest,
+  body: web::types::Json<serde_json::Value>,
+) -> HttpResponse {
+  println!("Hello req:  {req:#?}");
+  println!("Hello body:  {body:#?}");
+  HttpResponse::Ok().finish()
+}
+
+#[ntex::main]
+async fn main() -> std::io::Result<()> {
+  web::server(|| web::App::new().service(webhook))
+    .bind("0.0.0.0:8080")?
+    .run()
+    .await
 }
