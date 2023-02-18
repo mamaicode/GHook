a7365611134be59d3d2a6b319023a7835b2d0d75
scirin <irine.kokilashvili@gmail.com>
2023-02-18 17:35:56 +0000

    wip(HMAC): started sha-256 HMAC validation

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,32 @@
+use std::{env, arch::x86_64::_mm256_packus_epi16};
+
 use ntex::web::{self, HttpResponse, HttpRequest};
+
+use hmac_sha256::HMAC;
 
 #[web::post("/webhook")]
 async fn webhook(
   req: HttpRequest,
   body: web::types::Json<serde_json::Value>,
 ) -> HttpResponse {
-  println!("Hello req:  {req:#?}");
-  println!("Hello body:  {body:#?}");
+  let secret_token = env::var("SECRET_TOKEN)").unwrap();
+  // .map_err(|_| HttpResponse::InternalServerError().finish())?;
+  let signature_header = req.headers().get("X-Hub-Signature-256").unwrap();
+  // .ok_or(HttpResponse::InternalServerError().finish())?;
+
+  let payload_bytes = serde_json::to_string(&body.into_inner()).unwrap();
+  // .map_err(|_| HttpResponse::InternalServerError().finish())?;
+
+  let bitchleo = HMAC::mac(payload_bytes, secret_token);
+
+  let hex_string = bitchleo
+    .into_iter()
+    .map(|b| format!("{:02x}", b))
+    .collect::<String>();
+
+  println!("{}", hex_string);
+  println!("{:#?}", signature_header);
+
   HttpResponse::Ok().finish()
 }
 
